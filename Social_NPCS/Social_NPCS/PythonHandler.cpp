b5de7ba34491eeb2f05ced0a801ea15c51be42b5
Kyle Tuckey <malithium@gmail.com>
2017-01-23 07:43:48 +0000

    added some comments to the pythonHandler

--- Social_NPCS/Social_NPCS/PythonHandler.cpp
+++ Social_NPCS/Social_NPCS/PythonHandler.cpp
@@ -28,10 +28,10 @@ void PythonHandler::setFunctionName(std::string funcName)
 
 void PythonHandler::callPythonModule()
 {
-	//begin python
+	//Begin python
 	Py_Initialize();
 
-	//call the python module
+	//Call the python module
 	PyObject* module = PyImport_ImportModule(moduleName.c_str());
 	if (module == 0)
 	{
@@ -39,33 +39,33 @@ void PythonHandler::callPythonModule()
 		printf("Couldn't find python module");
 	}
 
-	//create a dictionary of the function's inside the module
+	//Create a dictionary of the function's inside the module
 	PyObject* comDict = PyModule_GetDict(module);
 
-	//state the specified function name from the dictionary
+	//State the specified function name from the dictionary
 	PyObject* func = PyDict_GetItemString(comDict, functionName.c_str());
 
-	//state the argument's you want NOTE: currently only work's with functions with 0 arguments
+	//State the argument's you want NOTE: currently only work's with functions with 0 arguments
 	PyObject* args = PyTuple_New(0);
 
-	//call the object
+	//Call the object
 	PyObject* result = PyObject_CallObject(func, args);
 	if (result == NULL)
 	{
 		printf("Calling function failed");
 	}
 
-	//print any errors
+	//Print any errors
 	PyErr_Print();
 
-	//clean up the pointer's
+	//Clean up the pointer's
 	Py_DECREF(module);
 	Py_DECREF(comDict);
 	Py_DECREF(func);
 	Py_DECREF(args);
 	Py_DECREF(result);
 
-	//end the python process
+	//End the python process
 	Py_Finalize();
 }
 
